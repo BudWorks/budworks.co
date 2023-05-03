b41e38ffbcbbf921983756168520e00c4c6422b5
JustYourBud <59189197+JustYourBud@users.noreply.github.com>
2023-05-02 23:38:55 -0500

    ✨ Add navigation buttons to Home
    Close #2
    Close #3
    
    Known issues:
    Buttons are not entirely responsive
    About button is offset from the other two

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
@@ -19,6 +20,21 @@ export default function Home() {
 					<div className={styles.onboarding}>
 						<div className={styles.onboarding__logo}>
 							<Image src="/svg/logos/budworks_stack.svg" alt="BudWorks" fill />
+						</div>
+						<div className={styles.onboarding__button}>
+							<Link href="/about">
+								<Image src="/svg/buttons/buttontop.svg" alt="About" fill />
+							</Link>
+						</div>
+						<div className={styles.onboarding__button}>
+							<Link href="/explore">
+								<Image src="/svg/buttons/buttonmiddle.svg" alt="Explore" fill />
+							</Link>
+						</div>
+						<div className={styles.onboarding__button}>
+							<Link href="#latest">
+								<Image src="/svg/buttons/buttonbottom.svg" alt="News" fill />
+							</Link>
 						</div>
 					</div>
 				</div>
